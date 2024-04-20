33d1d6283755ae0f2a0edf6061d7c33a087086f5
Pisarski Roman <133641494+Romanooo84@users.noreply.github.com>
2024-04-20 22:19:41 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,29 @@
 import { TextInput } from "./textinput";
+import { Contacts } from "./contacts";
+import React, { useState } from 'react';
 
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [name, setName] = useState('');
+  const [phoneNumber, setphoneNumber] = useState()
+
+  const InputData = (event) => {
+    event.preventDefault()
+    setName(event.target.value);
+  };
+  
+  const onSubmit = (event) => {
+    let id=contacts.length+1
+      setContacts(prevValue => [...prevValue, { name, id}]);
+    console.log(contacts)
+    console.log(contacts[0].name)
+    }
+
   return (
     <div>
-      <TextInput/>
+      <TextInput inputData={InputData} value={name} onSubmit={onSubmit} />
+      <Contacts contacts={contacts} />
     </div>
   );
-};
+  };
+  
--- src/components/textinput.jsx
+++ src/components/textinput.jsx
@@ -1,5 +1,21 @@
-export const TextInput = () => {
+export const TextInput = ({ inputData, value, onSubmit}) => {
+    
     return (
+    <div>  
         <p>Contacts</p>
+        <div> 
+            <p>Name</p>    
+                <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                    onChange={inputData}
+                />
+                <button onClick={onSubmit}>Add Contact</button>
+                <p>{value}</p>
+        </div> 
+    </div>
     )
 }
